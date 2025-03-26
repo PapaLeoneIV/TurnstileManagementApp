9c2aad24a34b9f77db423c4a5cf93a9786f3f25c
PapaLeoneIV <riccardo.leone97@gmail.com>
2025-03-26 11:26:37 +0100

    [logout feature]

--- turnstile-frontend/src/app/shared/components/header/header.component.ts
+++ turnstile-frontend/src/app/shared/components/header/header.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { LogoutComponent } from '../logout/logout.component';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LogoutComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
